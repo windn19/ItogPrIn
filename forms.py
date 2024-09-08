9780435d295ac8c3f7d0b0ecee2f21ce3fa25e68
windn1982 <windn18@yandex.ru>
2024-09-08 13:15:03 +0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -5,4 +5,5 @@ from wtforms.validators import DataRequired
 
 class FileForm(FlaskForm):
     file = FileField('Файл:', validators=[DataRequired()])
-    submit = SubmitField('Отправить')  
+    submit = SubmitField('Отправить')
+    
